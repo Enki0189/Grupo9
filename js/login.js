6d689b248f277b6cd311e59507598a10d274042d
Enki0189 <marudiaz0189@gmail.com>
2021-12-21 10:12:16 -0300

    agrego modificaciones a todo lo que es login

--- js/login.js
+++ js/login.js
@@ -2,20 +2,22 @@ function checkUserLogin(){
     let username = $('#username').val();
     let password = $('#password').val();
     if (username == '' || password == ''){
-        alert('Debe ingresar el usario y la contraseña para continuar');
+        alert('Debe ingresar el usuario y la contraseña para continuar');
     }else{
-        let url = "contrasena.json";
+        let url = "js/contraseña.json";
         $.getJSON( url , function( data ) {
-           // console.log(data);
-            sha256(password).then( function(respuestaHash){
-                checkUser(data, username, respuestaHash);
+            console.log(data);
+            sha256(password).then( function(respuestaHash) {
+                if(checkUser(data, username, respuestaHash)){
+                    window.location.href = "index.html";
+                }
                
-            }); 
-                
+            });
         }) 
         .fail(function() {
             alert( "error página en mantenimiento " );
         })
+        alert('Uusario y/o contraseña incorrectos');
     }
 }
 
@@ -25,17 +27,20 @@ function checkUser(data, username, password){
       
             val = JSON.stringify(val);
             val = JSON.parse(val);
-    
+
+            console.log('Username:' + username);
+            console.log('Username Json:' + val.user);
             if (username === val.user){
                 user = true;
-               
+                console.log('Password:' + password);
+                console.log('Password Json:' + val.password);
                 if (password == val.password){
                     console.log( "contraseña Correcta");
                     $('#userId').val(val.idUser);
                     $('#login_form').submit();
-                    return false;
+                    return true;
                 }else{
-                    alert( "contraseña incorrecta");
+
                     return false;
                 }
             }else {
@@ -43,9 +48,24 @@ function checkUser(data, username, password){
             }
         })
         
-        if (user == false){
-            console.log("Usuario no registrado");
-        }
+        return false;
        
    
+}
+
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    //console.log(hashHex);
+    return hashHex;
+
 }
